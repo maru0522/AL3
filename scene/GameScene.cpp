5436ea2768ffca3d696e6c368f5f3cbcc420dc33
maru0522 <k021g1258@g.neec.ac.jp>
2022-04-12 00:20:08 +0900

    授業資料を見ながら進めていたところ、スプライトの描画の実行結果の時点でエラーが発生。対処法不明のため一時中止として記録。

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,7 +6,9 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete sprite_;
+}
 
 void GameScene::Initialize() {
 
@@ -14,6 +16,11 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+
+	// ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("Resources/Task1_2Resources/mario.jpg");
+	// スプライトの生成
+	sprite_ = Sprite::Create(textureHandle_, { 100,50 });
 }
 
 void GameScene::Update() {}
@@ -56,6 +63,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
+	sprite_->Draw();
 
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);

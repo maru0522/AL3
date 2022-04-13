6ccd2bb481fd9365526bc9153094baa7cff77f62
maru0522 <k021g1258@g.neec.ac.jp>
2022-04-13 12:19:30 +0900

    KAMATAエンジンの使い方スプライトの描画/移動実行まで

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -18,12 +18,20 @@ void GameScene::Initialize() {
 	debugText_ = DebugText::GetInstance();
 
 	// ファイル名を指定してテクスチャを読み込む
-	textureHandle_ = TextureManager::Load("Resources/Task1_2Resources/mario.jpg");
+	textureHandle_ = TextureManager::Load("Task1_2Resources/mario.jpg");
 	// スプライトの生成
 	sprite_ = Sprite::Create(textureHandle_, { 100,50 });
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	// スプライトの今の座標を取得
+	XMFLOAT2 position = sprite_->GetPosition();
+	// 座標を{2,0}移動
+	position.x += 2.0f;
+	position.y += 1.0f;
+	// 移動した座標をスプライトに反映
+	sprite_->SetPosition(position);
+}
 
 void GameScene::Draw() {
 
